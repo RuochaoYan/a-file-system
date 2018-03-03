88c9af1339bf274cdf44d008617e5132e78a78a5
Alan' Yessenbayev <alan.yessenbayev@gmail.com>
2018-03-02 23:10:55 -0800

    Proposed interface for Archive,hpp

--- Archive.hpp
+++ Archive.hpp
@@ -2,24 +2,35 @@
 #define ARCHIVE_HPP_INCLUDED
 #include <vector>
 
-class block{
+class Block{
 public:
-    size_t blockSize() {return num_bytes;}
+    Block();
+    size_t size() {return num_bytes;}
     size_t next = 0; //the number of the next block, 0 if no next block
 protected:
     size_t num_bytes = 1024 // 1kByte blocks]
 };
 
-class file_entry{
+class FileEntry{
     char filename[32];
     char filetype[3];
     size_t size;
     size_t firstblock;
 };
 
-class repo{
+class Archive{
+public:
+    Archive(const char* name);
+    //~Archive(): might not need it yet.
+    add(const char* filename);
+    del(const char* filename);
+    list(const char* filename=nullptr);
+    find(const char* filename);
+    extract(const char* filename);
+
+protected:
     std::vector<block> files;
-    size_t size; //size in blocks
     std::vector<block> emptyblocks;
+    size_t size; //size in blocks
 };
 #endif // ARCHIVE_HPP_INCLUDED
