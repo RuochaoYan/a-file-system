93672f4ce15ce083ba1f915db12f4c0994ba8a7a
Alan' Yessenbayev <alan.yessenbayev@gmail.com>
2018-03-04 13:16:45 -0800

    Fixed bug in Archive.cpp

--- Archive.cpp
+++ Archive.cpp
@@ -8,8 +8,10 @@ Archive& Archive::add(std::string aFilename){
     std::vector<size_t> blocks; //vector of block number
     std::ifstream filetoAdd(aFilename,std::ifstream::ate|std::ifstream::binary);
     size_t fileSize=filetoAdd.tellg();
-    size_t blocknum = fileSize/1024; //number of blocks needed
-    for(size_t i=1; i<=blocknum; ++i) blocks.push_back(i); //trivially choosing successive blocks
+    size_t blocknum = fileSize/1024 + 1; //number of blocks needed
+    std::cout << "Filesize:" << fileSize <<  "\nNumber of Blocks: " << blocknum << std::endl;
+    for(size_t i=1; i<=blocknum; ++i) blocks.push_back(i); //trivially choosing successive blocks SHOULD BE MORE INTELLIGENT!!!
+    std::cout << "First Block:" << blocks[0] << std::endl;
     dir->append(aFilename,fileSize,blocks); //passing blocks to dir
     return *this;
 }
