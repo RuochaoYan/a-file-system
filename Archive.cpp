206a4052b976a6bfe20072d6718efef7cb961ea6
rjvkothari <aarjav.kothari@gmail.com>
2018-03-04 01:13:44 -0800

    Blocks fix

--- Archive.cpp
+++ Archive.cpp
@@ -7,7 +7,7 @@ Archive::Archive(std::string aName){
 Archive& Archive::add(std::string aFilename){
     std::ifstream filetoAdd(aFilename,std::ifstream::ate|std::ifstream::binary);
     size_t fileSize=filetoAdd.tellg();
-        dir->append(aFilename,fileSize,blocks);
+    dir->append(aFilename,fileSize,Blocks);
     return *this;
 }
 
