2c3eb18a8b3ebcf335c3953a60100e15931b74c0
shrivats1995 <35287303+shrivats1995@users.noreply.github.com>
2018-03-11 00:44:25 -0800

    updated tester

--- Tester.cpp
+++ Tester.cpp
@@ -1,31 +1,53 @@
 #include"Tester.hpp"
 
+void addDelTests()
+{
+    std::system("./sfarchiver add arc1 ./testfiles/test1.txt");
+
+    // test duplicate add case
+
+    std::system("./sfarchiver add arc1 ./testfiles/test1.txt");
+    std::system("./sfarchiver add arc1 ./testfiles/test1ddgd.txt");
+
+    // delete checks
+
+    std::system("./sfarchiver del arc1 test1.txt");
+    std::system("./sfarchiver del arc1 test1.txt");
+    std::system("./sfarchiver del arc1 google.png");
+
+    //add delete add deleted
+
+    std::system("./sfarchiver add arc1 ./testfiles/jfk.txt");
+    std::system("./sfarchiver del arc1 jfk.txt");
+    std::system("./sfarchiver add arc1 ./testfiles/jfk.txt");
+    std::system("./sfarchiver add arc1 ./testfiles/verne.txt");
+    std::system("./sfarchiver add arc1 ./testfiles/twain.txt");
+    std::system("./sfarchiver add arc1 ./testfiles/test1.txt");
+    std::system("./sfarchiver del arc1 twain.txt");
+    std::system("./sfarchiver add arc1 ./testfiles/google.png");
+    std::system("./sfarchiver del arc1 verne.txt");
+    std::system("./sfarchiver del arc1 google.png");
+    std::system("./sfarchiver add arc1 ./testfiles/testimg1.jpg");
+    std::system("./sfarchiver del arc1 testimg1.jpg");
+
+    //extract tests
+
+}
+void extractTests()
+{
+    std::system("./sfarchiver add arc2 ./testfiles/test1.txt");
+    std::system("./sfarchiver add arc2 ./testfiles/test2.txt");
+    std::system("./sfarchiver del arc2 test1.txt");
+    std::system("./sfarchiver extract arc2 test2.txt");
+    std::system("./sfarchiver del arc2 test2.txt");
+
+}
+
+
+
 void Tester:: runTests()
 {
-
-
-  std::system("./sfarchiver add arc1 ./testfiles/test1.txt");
-  // test duplicate add case
-  std::system("./sfarchiver add arc1 ./testfiles/test1.txt");
-  std::system("./sfarchiver add arc1 ./testfiles/test1ddgd.txt");
-  // delete checks
-  std::system("./sfarchiver del arc1 test1.txt");
-  std::system("./sfarchiver del arc1 test1.txt");
-  std::system("./sfarchiver del arc1 google.png");
-
-  //add delete add deleted
-  std::system("./sfarchiver add arc1 ./testfiles/jfk.txt");
-  std::system("./sfarchiver del arc1 jfk.txt");
-  std::system("./sfarchiver add arc1 ./testfiles/jfk.txt");
-  std::system("./sfarchiver add arc1 ./testfiles/verne.txt");
-  std::system("./sfarchiver add arc1 ./testfiles/twain.txt");
-  std::system("./sfarchiver add arc1 ./testfiles/test1.txt");
-  std::system("./sfarchiver del arc1 twain.txt");
-  std::system("./sfarchiver add arc1 ./testfiles/google.png");
-  std::system("./sfarchiver del arc1 verne.txt");
-  std::system("./sfarchiver del arc1 google.png");
-  std::system("./sfarchiver add arc1 ./testfiles/testimg1.jpg");
-  
-  std::system("./sfarchiver del arc1 testimg1.jpg");
+    addDelTests();
+    extractTests();
 
 }
